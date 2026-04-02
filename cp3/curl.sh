3270e782bd3ba534cbb81696c8f9a32db51df885
BasileLT <blt_git@pm.me>
2026-04-02 15:23:38 +0200

    curl

--- cp3/curl.sh
+++ cp3/curl.sh
@@ -4,3 +4,5 @@ curl -X POST "http://172.17.0.2:80/config"
 curl -X POST "http://172.17.0.2:80/validate"
 curl -X POST "http://172.17.0.2:80/check?id=1"
 curl -X POST "http://172.17.0.2:80/classify" -F "file=@ia/pictures/piano.jpg"
+
+open "http://172.17.0.2:80"
